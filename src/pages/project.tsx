9f0c037c2e98d400af9eef5d1afdb6faf8d798d5
Maxwell Kimoi <bowenmaxwell9@gmail.com>
2025-11-02 13:00:57 +0300

    enphasizing that the e-commerce is in development mode

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -33,7 +33,7 @@ export default function Projects() {
      {
       title: "E-Commerce Website",
       description:
-        "A modern e-commerce web app built with Next.js,Typescript, and Supabase — featuring seamless authentication, dynamic product listings, and a clean, responsive UI powered by Tailwind CSS and Framer Motion for smooth animations.",
+        "(In Development Process)A modern e-commerce web app built with Next.js,Typescript, and Supabase — featuring seamless authentication, dynamic product listings, and a clean, responsive UI powered by Tailwind CSS and Framer Motion for smooth animations.",
       tech: ["Next", "TypeScript", "Tailwind CSS","Supabase","Zustand","RHF + Zod","Framer Motion"],
       live: "https://e-commerce-keystore.vercel.app",
       github: "https://github.com/Keymoye/e-commerce",
